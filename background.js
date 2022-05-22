59ceb9f054467c3a4b058423466a1604687650df
Lorenzo Cao <lorenzocao25@gmail.com>
2022-05-22 01:22:10 -0700

    Initial commit

---
+++ background.js
@@ -0,0 +1,30 @@
+console.log('background.js running');
+
+
+const targetSite = ['bilibili.com'];
+
+const matchRule = (url) => {
+  for (const target of targetSite) {
+    if (url.includes(target)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+async function getCurrentTab() {
+  const queryOptions = { active: true, lastFocusedWindow: true };
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+chrome.tabs.onCreated.addListener(
+ async (tab) => {
+    const newPage = await getCurrentTab();
+    const url = newPage.pendingUrl;
+    if (matchRule(url)) {
+      chrome.tabs.remove(tab.id);
+    }
+    chrome.tabs.update({url: url});
+});
