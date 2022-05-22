12d5364d540cba10a10df2269ad94074fbc8322e
Lorenzo Cao <lorenzocao25@gmail.com>
2022-05-22 01:51:44 -0700

    Handle the case when user press shift/ctrl/command

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ console.log('background.js running');
 
 
 const targetSite = ['bilibili.com'];
+let shiftPressed = false, ctrlPressed = false;
 
 const matchRule = (url) => {
   for (const target of targetSite) {
@@ -12,19 +13,33 @@ const matchRule = (url) => {
   return false;
 };
 
+chrome.tabs.onCreated.addListener(
+ async (tab) => {
+    const newPage = await getCurrentTab();
+    const url = newPage.pendingUrl;
+    if (matchRule(url) && !shiftPressed) {
+      chrome.tabs.remove(tab.id);
+      chrome.tabs.update({url: url});
+    }
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+  switch(request.type){
+      case 'shiftPressed':
+          shiftPressed = true;
+          break;
+      case 'ctrlPressed':
+            ctrlPressed = true;
+            break;
+      case 'keyup':
+          shiftPressed = false;
+          break;
+  }
+}); 
+
 
 async function getCurrentTab() {
   const queryOptions = { active: true, lastFocusedWindow: true };
   const [tab] = await chrome.tabs.query(queryOptions);
   return tab;
 }
-
-chrome.tabs.onCreated.addListener(
- async (tab) => {
-    const newPage = await getCurrentTab();
-    const url = newPage.pendingUrl;
-    if (matchRule(url)) {
-      chrome.tabs.remove(tab.id);
-    }
-    chrome.tabs.update({url: url});
-});
